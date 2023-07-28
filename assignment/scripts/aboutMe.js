48587713e4323fb4f4b2fe74dd57066dfc0eb4b0
jcolago <jcolago2005@gmail.com>
2023-07-28 17:22:42 -0500

    add luck number switch statement

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -73,6 +73,24 @@ else {
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+int = luckyNumber
+switch (luckyNumber){
+    case 1:
+        console.log("First is the worst");
+        break;
+
+    case 2:
+        console.log("Second is the best");
+        break;
+    
+    case 3:
+        console.log("Third is the one with the polka dot dress");
+        break;
+
+    default:
+        console.log("Luck is what happens when preparation meets opportunity");
+        break;
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
