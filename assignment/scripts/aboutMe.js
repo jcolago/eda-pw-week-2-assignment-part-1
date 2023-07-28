1d599b603b9549953386d7c31a40d5178709f674
jcolago <jcolago2005@gmail.com>
2023-07-28 12:12:45 -0500

    add full name

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -7,9 +7,9 @@ let lastName = "Colago";
 console.log ( lastName )
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName = firstName + lastName ;
 // 4 - Console log the value of `fullName`
-
+console.log ( fullName )
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 
 // 6 - Console log this sentence, adding in the variables you created above: 
