91698dc63ac5d1503dc59598b634ae147555b4ce
jcolago <jcolago2005@gmail.com>
2023-07-28 17:50:05 -0500

    add console.log

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,8 +1,10 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = "Joe " ;
+console.log(firstName)
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = "Colago";
+console.log(lastName)
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName  +  lastName ;
@@ -10,22 +12,28 @@ let fullName = firstName  +  lastName ;
 console.log ( fullName ) ;
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 21 ;
+console.lot(luckyNumber)
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 console.log ("My name is " + fullName + " and I think " + luckyNumber +" is a winner!" ) ;
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = false ;
+console.log(adventurous)
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = "pansit" ;
+console.log(food)
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 0 ;
+console.log(pets)
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets = 1 ;
+console.log(friendsPets)
 // 11 - Add two pets to your `pets` variable
     pets += 2 ;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 const allowedPets = 2;
+console.log(allowedPets)
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 if (adventurous === true){
