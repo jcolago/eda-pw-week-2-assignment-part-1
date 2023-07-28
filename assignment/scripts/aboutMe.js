a65bf378e5053d0330ffec49244eeb3089d181b0
jcolago <jcolago2005@gmail.com>
2023-07-28 12:49:48 -0500

    add adventturous conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -28,8 +28,12 @@ let friendsPets = 1 ;
 const allowedPets = 2
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
-
+if (adventurous == true){
+    console.log( "Adventures are great!")
+}
+else{
+    console.log( "How about we stay home?")
+}
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
